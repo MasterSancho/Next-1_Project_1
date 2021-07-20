9197d7d6b7db488ebf26f10f1dc268ff4133d4ac
Alex M <1maltsevalex@gmail.com>
2021-07-20 22:25:52 +0300

    7. Components

--- content-manager-app/pages/index.js
+++ content-manager-app/pages/index.js
@@ -1,3 +1,29 @@
-export default function Home() {
- return <h1>Hello World</h1>;
+function CompA() {
+ return (
+  <>
+   <h1>CompA</h1>
+   <p>Hello Comp A</p>
+  </>
+ );
 }
+
+function CompB() {
+ return (
+  <>
+   <h1>CompB</h1>
+   <p>Hello Comp B</p>
+  </>
+ );
+}
+
+function Home() {
+ return (
+  <>
+   <h1>Hello World</h1>
+   <CompA />
+   <CompB />
+  </>
+ );
+}
+
+export default Home;
