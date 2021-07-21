01bfc60af4cf3d5e9ab386b354745f592a6eb616
Alex M <1maltsevalex@gmail.com>
2021-07-21 08:28:04 +0300

    14. Class state

--- content-manager-app/pages/index.js
+++ content-manager-app/pages/index.js
@@ -4,33 +4,48 @@ function CompA() {
  return (
   <>
    <h1>CompA</h1>
-   <CompB />
+   <p>Hello Comp A</p>
   </>
  );
 }
 
-function CompB() {
- return (
-  <>
-   <h1>CompB</h1>
-   <p>Hello Comp B</p>
-  </>
- );
+class CompC extends React.Component {
+ constructor() {
+  super();
+  this.state = {
+   myValue: 10,
+  };
+ }
+
+ changeState(incremenator) {
+  this.setState({
+   myValue: incremenator,
+  });
+ }
+
+ render() {
+  const myValue = this.state.myValue;
+
+  return (
+   <>
+    <h1>Hello CompC</h1>
+    Current Value: <h1>{this.state.myValue}</h1>
+    <button onClick={() => this.changeState(myValue + 1)}>+</button>
+    <button onClick={() => this.changeState(myValue - 1)}>-</button>
+   </>
+  );
+ }
 }
 
 const Home = () => {
  const [myValue, setValue] = useState(10);
-
- // const changeValue = (incrementor) => {
- //  setValue(myValue + incrementor);
- // };
 
  return (
   <>
    Current Value: <h1>{myValue}</h1>
    <button onClick={() => setValue(myValue + 1)}>+</button>
    <button onClick={() => setValue(myValue - 1)}>-</button>
-   <CompA />
+   <CompC />
   </>
  );
 };
